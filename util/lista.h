f34bbfb956b217d1c3e47a3167bb36336f85e540
Cpanighel <64702554+Cpanighel@users.noreply.github.com>
2020-05-26 18:43:03 +0200

    Aggiornato lista.h
    aggiornato lista.h con l'implementazione dei metodi

--- util/lista.h
+++ util/lista.h
@@ -27,40 +27,194 @@ public:
     //iteratore
     class constiterator {
         friend class lista<T>;
-      private:
+    private:
         nodo<T> *_ptr;
         bool _pastTheEnd; // true sse constiterator è "past-the-end"
         constiterator(nodo<T>* p, bool pte=false);
-      public:
+    public:
         constiterator(): _ptr(nullptr), _pastTheEnd(false) {}
 
-        T& operator*() const{
-          return _ptr->getInfo();
-        }
+        const T& operator*() const;
 
-        T* operator->() const{
-          return &(_ptr->getInfo());
-        }
+        const T* operator->() const;
 
         constiterator& operator++();
 
-        //++ postifisso
+        constiterator operator++(int);
 
         constiterator& operator--();
+
+        constiterator operator--(int);
 
         bool operator==(const constiterator& x) const;
 
         bool operator!=(const constiterator& x) const;
-      };
+    };
     constiterator begin() const;
 
     constiterator end() const;
 
-    T& front() const;
+    T front() const;
 
-    T& back() const;
-    
-    //manca il remove
+    T back() const;
+
 };
 
-#endif // LISTA_H
+template<class T>
+nodo<T> *lista<T>::copia(nodo<T> *f, nodo<T> *&l)
+{
+    if(f==nullptr) return l=nullptr; //this._end == nullptr => lista vuota
+    //la lista che sto copiando non è vuota
+    nodo<T>* prec=new nodo<T>(f->_info,nullptr,nullptr); //primo nodo
+    nodo<T>* succ=prec;
+    nodo<T>* start=prec; //nodo da cui si parte
+    while(f->_next!=nullptr) { //se il next non è nullo significa che ci sono almeno 2 nodi
+        succ = new nodo<T>(f->_next->_info,prec,nullptr);
+        prec->_next=succ;
+        f=f->_next;
+        prec=prec->_next;
+    }
+    l=succ; //l'_end viene aggiornato con il succ
+    return start;
+}
+
+template<class T>
+lista<T>::lista():_first(nullptr), _end(nullptr) {}
+
+template<class T>
+lista<T>::lista(const lista &l):_first(copia(l._first,_end)) {}
+
+template<class T>
+lista<T>::~lista() { delete _first; }
+
+template<class T>
+lista<T>& lista<T>::operator=(const lista &d) {
+    if(this != &d) {
+        delete _first;
+        _first=copia(d._first,_end);
+    }
+    return *this;
+}
+
+template<class T>
+bool lista<T>::isEmpty() const
+{
+    return (_first==nullptr);
+}
+
+template<class T>
+void lista<T>::insertFront(const T &t)
+{
+    nodo<T> *first=new nodo<T>(t);
+    if(this->isEmpty()) //lista vuota
+        _first=_end=first;
+    else //lista non vuota
+    {
+        first->_next=_first;
+        _first->_prev=first;
+        _first=first;
+    }
+}
+
+template<class T>
+void lista<T>::insertBack(const T &t)
+{
+    nodo<T> *last=new nodo<T>(t);
+    if(this->isEmpty()) //lista vuota
+        _first=_end=last;
+    else
+    {
+        last->_prev=_end;
+        _end->_next=last;
+        _end=last;
+    }
+}
+
+template<class T>
+typename lista<T>::constiterator lista<T>::begin() const {
+    return _first;
+}
+
+template<class T>
+typename lista<T>::constiterator lista<T>::end() const {
+    if(_end==nullptr) return nullptr;
+    return constiterator(_end+1,true); //ritorna il _pte sse lista non vuota
+}
+
+template<class T>
+T lista<T>::front() const {
+    return _first->getInfo();
+}
+
+template<class T>
+T lista<T>::back() const {
+    return _end->getInfo();
+}
+
+template<class T>
+lista<T>::constiterator::constiterator(nodo<T> *p, bool pte): _ptr(p), _pastTheEnd(pte) {}
+
+template<class T>
+const T& lista<T>::constiterator::operator*() const {
+    return _ptr->_info;
+}
+
+template<class T>
+const T* lista<T>::constiterator::operator->() const {
+    return &(_ptr->_info);
+}
+
+template<class T>
+typename lista<T>::constiterator& lista<T>::constiterator::operator++() {//prefisso
+    if(!_pastTheEnd)
+    {
+        if(_ptr->_next==nullptr) {++_ptr; _pastTheEnd=true;}
+        else
+            _ptr=_ptr->_next;
+    }
+    return *this;
+}
+
+template<class T>
+typename lista<T>::constiterator lista<T>::constiterator::operator++(int) {//postfisso
+    lista<T>::constiterator i = *this;
+    if(!_pastTheEnd)
+    {
+        if(_ptr->_next==nullptr) {++_ptr; _pastTheEnd=true;}
+        else
+            _ptr=_ptr->_next;
+    }
+    return i;
+}
+
+template<class T>
+typename lista<T>::constiterator& lista<T>::constiterator::operator--() {
+    if(_ptr!=nullptr)
+    {
+        if(_pastTheEnd) {--_ptr; _pastTheEnd=false;}
+        else _ptr=_ptr->_prev;
+    }
+    return *this;
+}
+
+template<class T>
+typename lista<T>::constiterator lista<T>::constiterator::operator--(int) {
+    lista<T>::constiterator i = *this;
+    if(_ptr!=nullptr)
+    {
+        if(_pastTheEnd) {--_ptr; _pastTheEnd=false;}
+        else _ptr=_ptr->_prev;
+    }
+    return i;
+}
+
+template<class T>
+bool lista<T>::constiterator::operator==(const typename lista<T>::constiterator& x) const {
+    return _ptr==x._ptr;
+}
+
+template<class T>
+bool lista<T>::constiterator::operator!=(const typename lista<T>::constiterator &x) const {
+    return !(_ptr==x._ptr);
+}
+#endif //LISTA_H
