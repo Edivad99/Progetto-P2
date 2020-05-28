e3d51e4182cf9cb1704ea14f283f29e433d29ba4
Alcky26 <64201984+Alcky26@users.noreply.github.com>
2020-05-28 16:26:50 +0200

    Lettura da csv
    Lettura fatta sul main

--- main.cpp
+++ main.cpp
@@ -10,6 +10,13 @@
 #include <model/persona.h>
 #include <model/studente_lavoratore.h>
 #include <util/lista.h>
+#include <util/telefono.h>
+#include <util/orelavorative.h>
+#include <QFile>
+#include <QStringList>
+#include <QString>
+#include <QTextStream>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
@@ -34,8 +41,90 @@ int main(int argc, char *argv[])
     std::cout << ((sl1 != sl2) ? "Vero" : "Falso") << std::endl;
     std::cout << sl1.getID() << std::endl;*/
 
-    return 0;
 
+    /*Letture da csv*/
+
+    QFile file("..\\Progetto-P2\\dipendenti.csv");
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << file.errorString();
+        return 1;
+    }
+
+    QStringList wordlist;
+    lista<Lavoratore*> dipendenti;
+    while (!file.atEnd()) {
+        QTextStream in(&file);
+        while (!in.atEnd())
+        {
+            QString line = in.readLine();
+            wordlist=line.split(";");
+            QString a=wordlist.first();
+            wordlist.pop_front();
+            if(a=="Operaio")
+            {
+                dipendenti.insertBack(new Operaio(wordlist[0].toUtf8().constData(),
+                                      wordlist[1].toUtf8().constData(),
+                        QDate(wordlist[2].split("/")[0].toInt(),wordlist[2].split("/")[1].toInt(),wordlist[2].split("/")[2].toInt()),
+                        wordlist[3].toUtf8().constData(),
+                        Genere(wordlist[4].toInt()),
+                        Telefono(wordlist[5].toUtf8().constData()),
+                        wordlist[7].toUtf8().constData(),
+                        OreLavorative(wordlist[8].split(":")[0].toInt(),wordlist[8].split(":")[1].toInt()),
+                        QDate(wordlist[9].split("/")[0].toInt(),wordlist[9].split("/")[1].toInt(),wordlist[9].split("/")[2].toInt()),
+                        Livello(wordlist[10].toInt())));
+            }
+            else
+            {
+                if(a=="Impiegato")
+                {
+                    dipendenti.insertBack(new Impiegato(wordlist[0].toUtf8().constData(),
+                                          wordlist[1].toUtf8().constData(),
+                            QDate(wordlist[2].split("/")[0].toInt(),wordlist[2].split("/")[1].toInt(),wordlist[2].split("/")[2].toInt()),
+                            wordlist[3].toUtf8().constData(),
+                            Genere(wordlist[4].toInt()),
+                            Telefono(wordlist[5].toUtf8().constData()),
+                            wordlist[7].toUtf8().constData(),
+                            OreLavorative(wordlist[8].split(":")[0].toInt(),wordlist[8].split(":")[1].toInt()),
+                            wordlist[11].toFloat(),
+                            QDate(wordlist[9].split("/")[0].toInt(),wordlist[9].split("/")[1].toInt(),wordlist[9].split("/")[2].toInt())));
+                }
+                else
+                {
+                    if(a=="Rappresentante")
+                    {
+                        dipendenti.insertBack(new Rappresentante(wordlist[0].toUtf8().constData(),
+                                              wordlist[1].toUtf8().constData(),
+                                QDate(wordlist[2].split("/")[0].toInt(),wordlist[2].split("/")[1].toInt(),wordlist[2].split("/")[2].toInt()),
+                                wordlist[3].toUtf8().constData(),
+                                Genere(wordlist[4].toInt()),
+                                Telefono(wordlist[5].toUtf8().constData()),
+                                wordlist[7].toUtf8().constData(),
+                                OreLavorative(wordlist[8].split(":")[0].toInt(),wordlist[8].split(":")[1].toInt()),
+                                wordlist[11].toFloat(),wordlist[12].toFloat(),
+                                QDate(wordlist[9].split("/")[0].toInt(),wordlist[9].split("/")[1].toInt(),
+                                wordlist[9].split("/")[2].toInt())));
+                    }
+                    else
+                    {
+                        if(a=="StudenteLavoratore")
+                        {
+                            dipendenti.insertBack(new StudenteLavoratore(wordlist[0].toUtf8().constData(),
+                                                  wordlist[1].toUtf8().constData(),
+                                    QDate(wordlist[2].split("/")[0].toInt(), wordlist[2].split("/")[1].toInt(),wordlist[2].split("/")[2].toInt()),
+                                    wordlist[3].toUtf8().constData(),
+                                    Genere(wordlist[4].toInt()),
+                                    Telefono(wordlist[5].toUtf8().constData()),
+                                    Occupazione(wordlist[6].toInt()),
+                                    wordlist[7].toUtf8().constData(),
+                                    OreLavorative(wordlist[8].split(":")[0].toInt(),wordlist[8].split(":")[1].toInt()),
+                                    QDate(wordlist[9].split("/")[0].toInt(), wordlist[9].split("/")[1].toInt(), wordlist[9].split("/")[2].toInt())));
+                        }
+                    }
+                }
+            }
+            wordlist.clear();
+        }
+    }
 
     MainWindow w;
     w.show();
