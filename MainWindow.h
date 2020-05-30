e8d95245b5e8e687ded16196f070d0e4ee52dab8
Davide Albiero <davide2910@live.it>
2020-05-30 17:22:01 +0200

    Spostato dove salvo il puntatore al QFile

--- MainWindow.h
+++ MainWindow.h
@@ -25,6 +25,8 @@ private:
     QVBoxLayout *mainLayout;
     QTabWidget *tabWidget;
 
+    QFile* fileAperto;
+
     TabellaModel *tabellaModel;
 
     void addMenuButtons();
--- model/tabellamodel.cpp
+++ model/tabellamodel.cpp
@@ -2,14 +2,12 @@
 
 TabellaModel::TabellaModel()
 {
-    //Imposto il file a nullo
-    currentFile = nullptr;
+
 }
 
 void TabellaModel::readFromFile(QFile *file)
 {
-    //Mi salvo il puntatore all'oggetto  file nel caso l'utente decida di fare solo salva
-    currentFile = file;
+
 }
 
 QDomDocument TabellaModel::saveFile()
