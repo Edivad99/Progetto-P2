0127588081b1e73c44325c1671aa85048e4d72fa
Davide Albiero <davide2910@live.it>
2020-05-30 18:40:39 +0200

    Tornato indietro

--- data/studente_lavoratore.cpp
+++ data/studente_lavoratore.cpp
@@ -22,8 +22,7 @@ float StudenteLavoratore::Stipendio(float bonus) const
 QDomElement StudenteLavoratore::XmlSerialize(QDomDocument doc)
 {
     QDomElement studlav = doc.createElement("StudenteLavoratore");
-    studlav.setAttribute("Occupazione", QString::fromStdString((getOccupazione() == 0) ? "Superiori" : "Universita'"));
-    //studlav.appendChild(Studente::XmlSerialize(doc));
+    studlav.appendChild(Studente::XmlSerialize(doc));
     studlav.appendChild(Lavoratore::XmlSerialize(doc));
     return studlav;
 }
