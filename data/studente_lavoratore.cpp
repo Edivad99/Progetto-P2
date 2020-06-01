8541a00524cb72c7b7c48f739f9d56911028014a
Davide Albiero <davide2910@live.it>
2020-06-01 17:13:26 +0200

    Aggiornato xml

--- data/studente_lavoratore.cpp
+++ data/studente_lavoratore.cpp
@@ -10,9 +10,9 @@ StudenteLavoratore::StudenteLavoratore(string nome, string cognome, QDate dataNa
 }
 
 StudenteLavoratore::StudenteLavoratore(QDomElement studlavor):
-    Persona(studlavor.childNodes().at(0).toElement()),//persona
-    Studente(studlavor.childNodes().at(1).toElement()),//studente
-    Lavoratore(studlavor.childNodes().at(2).toElement())//lavoratore
+    Persona(studlavor.childNodes().at(0).toElement().childNodes().at(0).toElement()),//persona
+    Studente(studlavor.childNodes().at(0).toElement()),//studente
+    Lavoratore(studlavor.childNodes().at(1).toElement())//lavoratore
 {
 
 }
@@ -32,14 +32,14 @@ QDomElement StudenteLavoratore::XmlSerialize(QDomDocument doc) const
     //creo un elemento studente lavoratore <StudenteLavoratore>
     QDomElement studlav = doc.createElement("StudenteLavoratore");
     //appendo <Persona>, come nella chiusura a diamante, l'elemento Persona viene creato da StudenteLavoratore
-    studlav.appendChild(Persona::XmlSerialize(doc));
+    //studlav.appendChild(Persona::XmlSerialize(doc));
     //creo elemento <Studente> e sottraggo la parte di persona
     QDomElement stud =Studente::XmlSerialize(doc);
-    stud.removeChild(stud.childNodes().at(0).toElement());
+    //stud.removeChild(stud.childNodes().at(0).toElement());
     studlav.appendChild(stud);
     //creo elemento <Lavoratore> e sottraggo la parte di persona
     QDomElement lavor =Lavoratore::XmlSerialize(doc);
-    lavor.removeChild(lavor.childNodes().at(0).toElement());
+    //lavor.removeChild(lavor.childNodes().at(0).toElement());
     studlav.appendChild(lavor);
     return studlav;
 }
